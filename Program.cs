90d8120487d7469a4006e2529782fbf45b2d4f17
Mohammed Mustafa <mjavax@gmail.com>
2023-01-15 21:46:32 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using ServiceReference;
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+iTrackClient cc = new iTrackClient();
+TrackRslt[] results = await cc.getSMSATrackingAsync("en","290385888574","username","password");
+Console.WriteLine(results);
